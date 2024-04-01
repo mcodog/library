637d25e6d4acb9afc2e6d784b5aeb7a6c853dd32
Nicole Duetes Bacala <markandrie.codog@tup.edu.ph>
2024-04-01 23:00:39 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@example.com',
+            'password' => 'secret123',
         ]);
     }
 }
